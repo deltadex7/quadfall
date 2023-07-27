07618a52a9b0de72a1430521dae91b976fdbc462
deltadex7 <54788149+deltadex7@users.noreply.github.com>
2023-07-27 09:16:54 +0700

    Add custom update/draw control logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #define SUPPORT_CUSTOM_FRAME_CONTROL 1
 
 #include <random>
+#include <chrono>
 #include <iostream>
 
 #include <raylib-cpp.hpp>
 
 #include "tfield.hpp"
 
+namespace chrono = std::chrono;
 
 using QC = qf::CellColor;
 using RC = raylib::Color;
+using intervalTimePoint = chrono::_V2::steady_clock::time_point;
+using durationDouble = chrono::duration<double>;
 
 int main() {
   
@@ -40,6 +44,34 @@ int main() {
   // seeded with system clock's epoch
   auto rand = std::mt19937_64(chrono::system_clock::now().time_since_epoch().count());
 
+  //----------------
+  // * logic update timer
+
+  // clock with fixed interval in nanoseconds
+  auto steadyClock = chrono::steady_clock();
+
+  const int targetUpdatePerSecond = 600;
+  using targetUpdateInterval = chrono::duration<int, std::ratio<1, targetUpdatePerSecond>>;
+  const auto targetUpdateTick = targetUpdateInterval(1);
+  // timepoint since last update
+  intervalTimePoint lastUpdate;
+  // timepoint at current update
+  intervalTimePoint currentUpdate;
+  // timepoint at next update
+  intervalTimePoint nextUpdate;
+
+  //----------------
+  // * graphics draw timer
+
+  const int targetDrawPerSecond = 60;
+  using targetDrawInterval = chrono::duration<int, std::ratio<1, targetDrawPerSecond>>;
+  const auto targetDrawTick = targetDrawInterval(1);
+  // timepoint since last draw
+  intervalTimePoint lastDraw;
+  // timepoint at current draw
+  intervalTimePoint currentDraw;
+  // timepoint at next draw
+  intervalTimePoint nextDraw;
 
   //----------------
   // * init values
@@ -51,11 +83,20 @@ int main() {
       },
     row2;
   
+  // some place to store the current time
+  intervalTimePoint currentTime;
+  // some place to store the previous time
+  intervalTimePoint lastTime;
+
+  // get the clock simultaneously so that we have no discrepancy
+  currentTime = lastUpdate = lastDraw = currentDraw = currentUpdate = nextUpdate = nextDraw = steadyClock.now();
+  
   #pragma endregion init
 
   // * MAIN LOOP PHASE * //
   while (!w.ShouldClose()) // Detect window close button or ESC key
   {
+    // TODO implement polling, buffer swap, and wait
 
     //----------------
     // * UPDATE
@@ -63,7 +104,14 @@ int main() {
     
     const char *text;
 
+    if (currentTime >= nextUpdate) {
       // perform update logic
+
+      //----------------
+      // * POLLING
+      #pragma region polling
+      PollInputEvents();
+      #pragma endregion polling
 
       timer--;
       std::cout << timer << "\n";
@@ -79,12 +127,33 @@ int main() {
 
       text = row1.full() ? "row is full" : "row is not full";
 
+      // update the update tick
+
+      currentUpdate = steadyClock.now();
+      auto updateTimeDif = chrono::duration_cast<durationDouble>(currentUpdate - lastUpdate);
+
+      bool updateDirty = false;
+      while (nextUpdate <= currentTime) {
+        nextUpdate += chrono::duration_cast<chrono::steady_clock::duration>(targetUpdateTick);
+
+        if (updateDirty)
+          std::cout << "Skipping update tick, input may be dropped, logic is delayed.\n";
+        
+        updateDirty = true;
+      }
+
+      std::cout << "Time to update: " << updateTimeDif.count() << "\n"
+                << "Current UPS   : " << 1 / updateTimeDif.count() << "\n";
+
+      lastUpdate = currentUpdate;
+    }
 
     #pragma endregion update
 
     //----------------
     // * DRAW
     #pragma region draw
+    if (currentTime >= nextDraw) {
       // perform update logic
       BeginDrawing();
       {
@@ -93,10 +162,59 @@ int main() {
         row1.draw(100,200,64);
       }
       EndDrawing();
+      SwapScreenBuffer();
 
+      // update the update tick
+
+      currentDraw = steadyClock.now();
+      auto drawTimeDif = chrono::duration_cast<durationDouble>(currentDraw - lastDraw);
+
+      nextDraw = currentDraw;
+      nextDraw += chrono::duration_cast<chrono::steady_clock::duration>(targetDrawTick);
+
+      std::cout << "Time to draw: " << drawTimeDif.count() << "\n"
+                << "Current DPS   : " << 1 / drawTimeDif.count() << "\n";
+
+      lastDraw = currentDraw;
+    }
     #pragma endregion draw
 
+    //----------------
+    // * WAIT
+    #pragma region wait
+    auto compareUpdateDraw = nextUpdate <=> nextDraw;
+    currentTime = steadyClock.now();
+    durationDouble waitTime;
+
+    std::cout << "Current time: " << currentTime.time_since_epoch().count() << "\n"
+              << "Next update : " << nextUpdate.time_since_epoch().count() << "\n"
+              << "Next draw   : " << nextDraw.time_since_epoch().count() << "\n";
+
+    if (compareUpdateDraw < 0) { // update first
+      waitTime = chrono::duration_cast<durationDouble>(nextUpdate-currentTime);
+      std::cout << "Next update in: ";
+    } else if (compareUpdateDraw > 0) { // draw first
+      waitTime = chrono::duration_cast<durationDouble>(nextDraw-currentTime);
+      std::cout << "Next draw in: ";
+    } else { // update and draw
+      waitTime = chrono::duration_cast<durationDouble>(nextUpdate-currentTime);
+      std::cout << "Next update+draw in: ";
     }
+    auto wtc = waitTime.count();
+    if (wtc > 0.0) {
+      std::cout << wtc * 1000 << "\n";
+      WaitTime(wtc);
+    } else {
+      std::cout << "0 (rectified from " << wtc * 1000 << ")\n";
+    }
+    currentTime = steadyClock.now();
+    deltaTime = chrono::duration_cast<durationDouble>(currentTime - lastTime);
+    std::cout << "Actual wait time:" << deltaTime.count() * 1000 << "\n";
+    lastTime = currentTime;
+
+    #pragma endregion wait
+  }
+
   // * DEINIT PHASE * //
   #pragma region deinit
 
